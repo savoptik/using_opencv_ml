b98df1954f9563d1c4c95ef06f6d2f0dab08f76d
savoptik <sav70792@gmail.com>
2018-12-23 15:54:05 +0300

    Initial Commit

---
+++ using_opencv_l/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  using_opencv_l
+//
+//  Created by Артём Семёнов on 23/12/2018.
+//  Copyright © 2018 Артём Семёнов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
