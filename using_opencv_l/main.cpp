b47ecf73b5b3eb2736c906cc807e268be19ced02
savoptik <sav70792@gmail.com>
2018-12-23 16:37:28 +0300

    Выполнено преобразование изображений MNIST в изображения cv::Mat.

--- using_opencv_l/main.cpp
+++ using_opencv_l/main.cpp
@@ -7,9 +7,31 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/ml.hpp>
+#include <opencv2/dnn.hpp>
+#include <mnist/mnist_reader.hpp>
+
+void conversionMNISTToCVMAT(std::vector<std::vector<unsigned char>>& imgs, std::vector<cv::Mat>& res);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    auto ds = mnist::read_dataset(); // читаем базу мнист
+    std::vector<cv::Mat> trainImages, testImages; // так как объекты ML работают с cv::Mat
+    conversionMNISTToCVMAT(ds.training_images, trainImages); // приобразовываем изображения
+    conversionMNISTToCVMAT(ds.test_images, testImages);
+    std::cout << "преобразованно " << ds.training_images.size() << " тренеровочных примеров в " << trainImages.size() << " примеров\n";
+    std::cout << "Преобразованно " << ds.test_images.size() << " тестовых примеров в " << testImages.size() << " примеров\n";
     return 0;
+}
+
+void conversionMNISTToCVMAT(std::vector<std::vector<unsigned char>>& imgs, std::vector<cv::Mat>& res) {
+    for (auto img : imgs) {
+        cv::Mat tIMG(28, 28, CV_8UC1);
+        tIMG.data = img.data();
+        res.push_back(tIMG);
+    }
 }
