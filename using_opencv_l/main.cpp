c0030043f497fd5c20a46f238915a2bb753a514d
savoptik <sav70792@gmail.com>
2019-01-19 23:53:45 +0300

    Внесены данные в файл md

--- using_opencv_l/main.cpp
+++ using_opencv_l/main.cpp
@@ -23,19 +23,21 @@ int main(int argc, const char * argv[]) {
     std::cout << "Имеем " << trainResponse.rows << " откликов размером в" << trainResponse.cols << std::endl;
     auto trainlables = traindata->getClassLabels();
     std::cout << "Имеем " << trainlables.rows << " меток размером в" << trainlables.cols << std::endl;
+    for (int i = 0; i < trainlables.rows; i++) {
+        std::cout << trainlables.at<short>(i, 0) << " ";
+    } std::cout << std::endl;
     // разделение выборок на тестовую и обучающую
     traindata->setTrainTestSplit(18000);
     auto ts = traindata->getTestSamples();
     std::cout << "Получено " << ts.rows << " тестовых примеров\n";
-
     // машина опорных векторов
     auto svm = cv::ml::SVM::create();
     svm->setType(cv::ml::SVM::C_SVC);
     svm->setKernel(cv::ml::SVM::LINEAR);
-    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 1000, 1e-9));
     svm->trainAuto(traindata);
     cv::Mat outresponces(2000, 1, CV_32F);
-    float err = svm->calcError(traindata, false, outresponces);
+    float err = svm->calcError(traindata, true, outresponces);
     std::cout << "Ошибка предсказания " << err << std::endl;
     return 0; // выход
 }
