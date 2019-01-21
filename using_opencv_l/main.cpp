75db9ade93dde66b0a9b5cf922e90cbab043973d
savoptik <sav70792@gmail.com>
2019-01-21 19:15:12 +0300

    SVM выделен в отдельную ветку

--- using_opencv_l/main.cpp
+++ using_opencv_l/main.cpp
@@ -32,48 +32,12 @@ int main(int argc, const char * argv[]) {
     auto ts = traindata->getTestSamples();
     std::cout << "Получено " << ts.rows << " тестовых примеров\n";
     // машина опорных векторов
-/*    auto svm = cv::ml::SVM::create();
+    auto svm = cv::ml::SVM::create();
     svm->setType(cv::ml::SVM::C_SVC);
     svm->setKernel(cv::ml::SVM::LINEAR);
     svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 1000, 1e-9));
     svm->trainAuto(traindata);
     float err = svm->calcError(traindata, true, outresponces);
-    std::cout << "Ошибка предсказания " << err << std::endl; */
-    // многослойный персептрон
-/*    auto mlp = cv::ml::ANN_MLP::create();
-    cv::Mat ls({datatrayn.cols, 8, datatrayn.cols});
-    mlp->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
-    mlp->setLayerSizes(ls);
-    mlp->setActivationFunction(cv::ml::ANN_MLP::ActivationFunctions::SIGMOID_SYM);
-    mlp->setTrainMethod(cv::ml::ANN_MLP::BACKPROP);
-    cv::Mat newtrainresponce(trainResponse.rows, datatrayn.cols, CV_32F);
-    newtrainresponce = newtrainresponce * 0;
-    for (int i = 0; i < newtrainresponce.rows; i++) {
-        newtrainresponce.at<float>(i, trainResponse.at<float>(i)) = 1;
-    }
-    mlp->train(datatrayn, cv::ml::ROW_SAMPLE, newtrainresponce);
-    float err = mlp->calcError(traindata, true, newtrainresponce);
-    std::cout << "ошибка предсказания " << err << std::endl; */
-    // бустинг
-/*    auto boosting = cv::ml::Boost::create();
-    boosting->setBoostType(cv::ml::Boost::Types::REAL);
-    boosting->setWeakCount(200);
-    boosting->setWeightTrimRate(0);
-    boosting->train(traindata);
-    float err = boosting->calcError(traindata, true, outresponces);
-    std::cout << "Ошибка для бустинга " << err << std::endl; */
-    // случайный лес
-    /*    auto randomForest = cv::ml::RTrees::create();
-    randomForest->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
-    cv::Mat newtrainresponce(trainResponse.rows, datatrayn.cols, CV_32F);
-    randomForest->setActiveVarCount(4);
-    randomForest->train(traindata);
-    std::cout <<"Ошибка случайного леса " << randomForest->calcError(traindata, true, outresponces); */
-    // к ближайших соседей
-/*    auto kNearest = cv::ml::StatModel::train<cv::ml::KNearest>(traindata);
-    std::cout << "Ошибка к соседей " << kNearest->calcError(traindata, true, outresponces) << " при " << kNearest->getDefaultK() << " соседях по умолчанию и " << std::endl; */
-    // нормальный байесовский классификатор
-/*    auto nbc = cv::ml::StatModel::train<cv::ml::NormalBayesClassifier>(traindata);
-    std::cout << " ошибка нормального байесовского классификатора " << nbc->calcError(traindata, true, outresponces) << std::endl; */
+    std::cout << "Ошибка предсказания " << err << std::endl;
     return 0; // выход
 }
