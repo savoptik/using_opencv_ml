f39523a47682edcd3f2ec56e85c1849891298b96
savoptik <sav70792@gmail.com>
2019-01-21 19:20:07 +0300

    Многослойный персептрон вынесен в отдельную ветку

--- using_opencv_l/main.cpp
+++ using_opencv_l/main.cpp
@@ -31,16 +31,8 @@ int main(int argc, const char * argv[]) {
     traindata->setTrainTestSplit(18000);
     auto ts = traindata->getTestSamples();
     std::cout << "Получено " << ts.rows << " тестовых примеров\n";
-    // машина опорных векторов
-/*    auto svm = cv::ml::SVM::create();
-    svm->setType(cv::ml::SVM::C_SVC);
-    svm->setKernel(cv::ml::SVM::LINEAR);
-    svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 1000, 1e-9));
-    svm->trainAuto(traindata);
-    float err = svm->calcError(traindata, true, outresponces);
-    std::cout << "Ошибка предсказания " << err << std::endl; */
     // многослойный персептрон
-/*    auto mlp = cv::ml::ANN_MLP::create();
+    auto mlp = cv::ml::ANN_MLP::create();
     cv::Mat ls({datatrayn.cols, 8, datatrayn.cols});
     mlp->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
     mlp->setLayerSizes(ls);
@@ -53,27 +45,6 @@ int main(int argc, const char * argv[]) {
     }
     mlp->train(datatrayn, cv::ml::ROW_SAMPLE, newtrainresponce);
     float err = mlp->calcError(traindata, true, newtrainresponce);
-    std::cout << "ошибка предсказания " << err << std::endl; */
-    // бустинг
-/*    auto boosting = cv::ml::Boost::create();
-    boosting->setBoostType(cv::ml::Boost::Types::REAL);
-    boosting->setWeakCount(200);
-    boosting->setWeightTrimRate(0);
-    boosting->train(traindata);
-    float err = boosting->calcError(traindata, true, outresponces);
-    std::cout << "Ошибка для бустинга " << err << std::endl; */
-    // случайный лес
-    /*    auto randomForest = cv::ml::RTrees::create();
-    randomForest->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
-    cv::Mat newtrainresponce(trainResponse.rows, datatrayn.cols, CV_32F);
-    randomForest->setActiveVarCount(4);
-    randomForest->train(traindata);
-    std::cout <<"Ошибка случайного леса " << randomForest->calcError(traindata, true, outresponces); */
-    // к ближайших соседей
-/*    auto kNearest = cv::ml::StatModel::train<cv::ml::KNearest>(traindata);
-    std::cout << "Ошибка к соседей " << kNearest->calcError(traindata, true, outresponces) << " при " << kNearest->getDefaultK() << " соседях по умолчанию и " << std::endl; */
-    // нормальный байесовский классификатор
-/*    auto nbc = cv::ml::StatModel::train<cv::ml::NormalBayesClassifier>(traindata);
-    std::cout << " ошибка нормального байесовского классификатора " << nbc->calcError(traindata, true, outresponces) << std::endl; */
+    std::cout << "ошибка предсказания " << err << std::endl;
     return 0; // выход
 }
